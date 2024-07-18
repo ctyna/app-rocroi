06c66a5b90fd8e41ca67667c377a29586f7eedf8
ctyna <tynakles@alumnos.cei.es>
2024-07-18 13:01:02 +0200

    prueba header

--- Components/Header/Header.jsx
+++ Components/Header/Header.jsx
@@ -52,10 +52,13 @@ export const Header = () => {
                             <li className="Nav-li">
                                 <NavLink to='/reservas'>Reservas</NavLink>
                             </li>
-                            <li onClick={cerrarSesión} className="Nav-li">
+                           
+
+                        </ul>
+                        <ul className="Nav-ul">
+                        <li onClick={cerrarSesión} className="Nav-li">
                                 Cerrar sesión
                             </li>
-
 
                         </ul>
                     </nav>
