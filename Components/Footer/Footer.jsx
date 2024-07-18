5bceb49b26ae03634e734453f2e1752e42e9fdd2
ctyna <tynakles@alumnos.cei.es>
2024-07-18 12:21:29 +0200

    problemas width

--- Components/Footer/Footer.jsx
+++ Components/Footer/Footer.jsx
@@ -11,9 +11,9 @@ export const Footer = () => {
             <footer className="Footer">
                 <div className="Footer-wrapper">
                     <div className="Footer-awards">
-                        <img className='Footer-img' width={279} height={267} src={aenorImage} alt="Sello de calidad aenor" title='aenor' loading='lazy' />
-                        <img className='Footer-img' width={200} height={200} src={biosphereImage} alt="Sello calidad Biosphere" title='Biosphere logo' loading='lazy' />
-                        <img className='Footer-img' width={500} height={500}  src={travelersImage} alt="travelers choise 2023 " title='premio travelers choise' loading='lazy' />
+                        <img className='Footer-img' width={100} height={100} src={aenorImage} alt="Sello de calidad aenor" title='aenor' loading='lazy' />
+                        <img className='Footer-img' width={100} height={100} src={biosphereImage} alt="Sello calidad Biosphere" title='Biosphere logo' loading='lazy' />
+                        <img className='Footer-img' width={100} height={100}  src={travelersImage} alt="travelers choise 2023 " title='premio travelers choise' loading='lazy' />
 
                     </div>
                     <div className="Footer-links">
